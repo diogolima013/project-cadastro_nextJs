62acdba649402e1acbfae3c2e7afd990de84e33d
Diogo <diogoslva0133@gmail.com>
2022-11-04 07:41:15 -0300

    add: fragment component

---
+++ src/pages/exemplo.jsx
@@ -0,0 +1,10 @@
+import Cabecalho from "../components/Cabecalho";
+
+export default function Exemplo() {
+    return (
+        <>
+         <Cabecalho/>
+         <Cabecalho/>
+       </> //js fragment, dessa forma consigo retorna dois elementos 
+    )
+}
